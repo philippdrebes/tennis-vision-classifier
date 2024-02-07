092bf6a1ec40dd6970325262651d098abf609105
Philipp Drebes <philipp.drebes@gmail.com>
2024-02-07 10:13:03 +0100

    modified extract_frames.sh

--- extract_frames.sh
+++ extract_frames.sh
@@ -11,7 +11,9 @@ mkdir -p "$OUTPUT_DIR"
 
 # Iterate over each video file in the video directory
 for video in "$VIDEO_DIR"/*.mp4; do
+  VIDEO_OUTPUT_DIR="${OUTPUT_DIR}/$(basename "$video" .mp4)"
+  mkdir -p "$VIDEO_OUTPUT_DIR"
   # Use FFmpeg to extract one frame per second
-  ffmpeg -i "$video" -vf "fps=1" "$OUTPUT_DIR/$(basename "$video" .mp4)_frame_%04d.jpg"
+  ffmpeg -i "$video" -vf "fps=1" "$VIDEO_OUTPUT_DIR/$(basename "$video" .mp4)_frame_%04d.jpg"
 done
 
